65a5050b17e27deed4bd7b37e44f468cfe9c317a
mvermadev <manishverma001997@gmail.com>
2020-04-15 19:13:54 +0530

    minor

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Charts from './components/Charts';
 import Header from './components/Header';
 import Regional from './components/Regional';
-import Demo from './components/Demo';
 import Footer from './components/Footer';
 
 function Main(props) {
