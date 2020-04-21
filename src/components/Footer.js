c21e19279058c35f59cad01cf486b9dfde7828e5
mvermadev <manishverma001997@gmail.com>
2020-04-21 16:36:13 +0530

    mnr

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(props) {
     return (
         <div>
-            <p style={{textAlign: 'right', opacity: '0.9', paddingRight: '20px', marginTop: '10px'}}>Created by: <a href="https://www.instagram.com/coder.mverma/" target="blank">Manish Verma</a> TY BCA.</p>
+            <p style={{textAlign: 'right', opacity: '0.9', paddingRight: '20px', marginTop: '10px', fontSize: '14px'}}>Created by: <a href="https://www.instagram.com/coder.mverma/" target="blank">Manish Verma</a> TY BCA.</p>
         </div>
     );
 }
